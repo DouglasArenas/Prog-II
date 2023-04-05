14378b8ddd0a63d11cab19a8c5f86655d0ad7831
Douglas <douglasarenas71@gmail.com>
2023-04-05 11:54:55 -0300

    Updating something

--- calculadora/Calculator/src/Validator.java
+++ calculadora/Calculator/src/Validator.java
@@ -3,6 +3,15 @@ public class Validator {
         return !Double.isNaN(num) && !Double.isInfinite(num);
     }
 
+    public static boolean isValidNumberOfTerm(String numStr) {
+        try {
+            double num = Double.parseDouble(numStr);
+            return !Double.isNaN(num) && !Double.isInfinite(num);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
     public static boolean isValidOperator(String operator) {
         return operator.matches("[+\\-*/]");
     }
